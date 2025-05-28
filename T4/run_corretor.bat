9a4e47a3c2cbef0cf03ac36166301a58431ffd33
Pedro <88565386+void-Pedro@users.noreply.github.com>
2025-05-27 22:07:22 -0300

    Update run_corretor.bat

--- T4/run_corretor.bat
+++ T4/run_corretor.bat
@@ -1,3 +1,3 @@
 cd ./alguma-semantico
 mvn clean package
-java -jar ../corretor/compiladores-corretor-automatico-1.0-SNAPSHOT-jar-with-dependencies.jar  "java -jar ./target/alguma-semantico-1.0-SNAPSHOT-jar-with-dependencies.jar" gcc ../corretor/temp/ ../corretor/casos-de-teste/ "804071, 791085" t3
+java -jar ../corretor/compiladores-corretor-automatico-1.0-SNAPSHOT-jar-with-dependencies.jar  "java -jar ./target/alguma-semantico-1.0-SNAPSHOT-jar-with-dependencies.jar" gcc ../corretor/temp/ ../corretor/casos-de-teste/ "804071, 791085" t4
